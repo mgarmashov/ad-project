62cbbcac480a78a364f8408ab2006d32a3c43aa6
mgarmashov <mikhail.garmashov@gmail.com>
2018-12-21 14:16:26 +0500

    Урок 10. Добавлен список заказов и форма новой рекламы

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,60 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>New Ad</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3">Create new ad</h1>
+                <v-form v-model="valid" ref="form" validation class="mb-3">
+                    <v-text-field
+                            name="title"
+                            label="Ad title"
+                            type="text"
+                            v-model="title"
+                            required
+                            :rules="[v => !!v || 'Поле обязательное']"
+                    ></v-text-field>
+                    <v-textarea
+                            name="description"
+                            label="Ad desc"
+                            type="text"
+                            v-model="description"
+                            required
+                            :rules="[v => !!v || 'Поле обязательное']"
+                            multi-line
+                    ></v-textarea>
+                </v-form>
+                <v-layout row mb-3 >
+                    <v-flex xs12>
+                        <v-btn
+                                class="warning"
+                        >
+                            Upload
+                            <v-icon right dark>cloud_upload</v-icon>
+                        </v-btn>
+                    </v-flex>
+                </v-layout>
+                <v-layout row>
+                    <v-flex xs12>
+                        <img src="" alt="" height="100">
+                    </v-flex>
+                </v-layout>
+
+                <v-layout row>
+                    <v-flex xs12>
+                        <v-switch
+                            :label="'Показывать в слайдере'"
+                            v-model="promo"
+                            color="primary"
+                        >
+                        </v-switch>
+                    </v-flex>
+                </v-layout>
+
+                <v-layout row>
+                    <v-flex xs12>
+                        <v-spacer></v-spacer>
+                        <v-btn class="success" @click="createAd" :disabled="!valid">Создать</v-btn>
+                    </v-flex>
+                </v-layout>
             </v-flex>
         </v-layout>
     </v-container>
@@ -11,7 +63,25 @@
 <script>
   export default {
     data () {
-      return {}
+      return {
+        title: '',
+        description: '',
+        promo: false,
+        valid: false
+      }
+    },
+    methods: {
+      createAd () {
+        if (this.$refs.form.validate()) {
+          const ad = {
+            title: this.title,
+            description: this.description,
+            promo: this.promo
+          }
+
+          console.log(ad)
+        }
+      }
     }
   }
 </script>
